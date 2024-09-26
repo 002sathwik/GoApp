4d9e07e57b0a565b0d30f00947aaa9a000f76d8a
Sathwik <hsathwik002@gmail.com>
2024-09-26 12:39:30 +0530

    feath:array

--- pointers/main.go
+++ pointers/main.go
@@ -8,19 +8,30 @@ func main() {
 	// var ptr *int
 	// fmt.Println("Value of ptr: ", ptr)
 
-	myNumber := 23
+	// myNumber := 23
 
-	var ptr *int = &myNumber
+	// var ptr *int = &myNumber
 
-	fmt.Println("Value of myNumber: ", ptr)
-	fmt.Println("Value of myNumber: ", myNumber)
-	fmt.Println("Value of myNumber: ", *ptr)
-	fmt.Println("Value of myNumber: ", &myNumber)
+	// fmt.Println("Value of myNumber: ", ptr)
+	// fmt.Println("Value of myNumber: ", myNumber)
+	// fmt.Println("Value of myNumber: ", *ptr)
+	// fmt.Println("Value of myNumber: ", &myNumber)
 
-	*ptr = *ptr * 2
+	// *ptr = *ptr * 2
 
-	fmt.Println("Value of myNumber: ", myNumber)
-	fmt.Println("Value of myNumber: ", ptr)
-	fmt.Println("Value of myNumber: ", *ptr)
+	// fmt.Println("Value of myNumber: ", myNumber)
+	// fmt.Println("Value of myNumber: ", ptr)
+	// fmt.Println("Value of myNumber: ", *ptr)
 
+	//Array
+
+	var arr [5]int
+
+	arr[0] = 1
+	arr[1] = 12
+	arr[2] = 13
+	arr[3] = 14
+
+	fmt.Println("Value of arr: ", arr)
+	fmt.Println(" Len  of arr: ", len(arr))
 }
