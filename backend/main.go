76756e35bfb4952f0ffbcf2497a20c9b4ff91c3f
Sathwik <hsathwik002@gmail.com>
2024-09-29 09:58:40 +0530

    feath:postRequest

--- backend/main.go
+++ backend/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Init function")
-	PerformGetRequest()
+	// PerformGetRequest()
+	PerformPostRequest()
 }
 
 func PerformGetRequest() {
@@ -23,8 +25,33 @@ func PerformGetRequest() {
 
 	fmt.Println("Response status:", res.StatusCode)
 	fmt.Println("Content Length:", res.ContentLength)
+
+	var responseString strings.Builder
 	con, _ := ioutil.ReadAll(res.Body)
+	byteCount, _ := responseString.Write(con)
+	fmt.Println(byteCount)
 
-	fmt.Println(string(con))
+	// fmt.Println(string(con))
 
+}
+
+func PerformPostRequest() {
+	const myurl = "http://localhost:3000/post"
+
+	requestBody := strings.NewReader(`
+	{
+	 "name":" sathwik",
+	 "age": 22,
+	 "city": "bangalore",
+	 "email": "fshaghsu@gmail.com"
+	}
+	`)
+
+	res, err := http.Post(myurl, "application/json", requestBody)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	cont, err := ioutil.ReadAll(res.Body)
+	fmt.Println(string(cont))
 }
